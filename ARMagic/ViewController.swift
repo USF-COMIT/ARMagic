31cbadbd9951eb39211cb9ff7b32ce08d1e8241b
darshangummadi <98764752+darshangummadi@users.noreply.github.com>
2023-09-07 22:48:58 -0400

    Initial Commit

---
+++ ARMagic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARMagic
+//
+//  Created by Darshan Gummadi on 9/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
